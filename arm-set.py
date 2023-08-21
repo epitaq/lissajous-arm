f328a9d60cb435ac5cd7effd72f4f1f2e0ff39bd
epita <somaok2004@gmail.com>
2023-08-22 01:33:44 +0900

    アーム取付用に初期位置に動かす

---
+++ arm-set.py
@@ -0,0 +1,16 @@
+import arm
+
+SERVO_CHANNELS = {
+    'root_servo': 0,
+    'head_servo': 4,
+    'root_head_servo': 8,
+    'root_link_servo': 12
+}
+ARM_LENGTHS = {
+    'head_arm_length': 200,
+    'root_head_arm_length': 200,
+    'root_link_arm_length': 50
+}
+arm = arm.Arm(SERVO_CHANNELS, ARM_LENGTHS)
+print(arm.getServoAngles())
+# arm.setRotationRadius(200)
